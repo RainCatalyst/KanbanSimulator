9ac491ef2c435347d105e88b47e2670319e08934
shabalin13 <d.shabalin@innopolis.university>
2021-07-01 15:44:23 +0300

    обновление

--- kanban_simulator/board/forms.py
+++ kanban_simulator/board/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class CreateRoomForm(forms.Form):
     name = forms.CharField(label='Ваш никнейм.', max_length=20)
-    teams_num = forms.IntegerField(label='Количество команд.')
+    teams_num = forms.IntegerField(label='Количество команд.', min_value=0, max_value=10)
     spectator = forms.BooleanField(label='Хотите ли вы быть наблюдателем?', required=False)
 
 
